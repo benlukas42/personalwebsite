edf10574958a256e1b474d1c05480ed2a287726e
benlukas42 <97489626+benlukas42@users.noreply.github.com>
2024-01-17 19:44:06 -0600

    Fixed metadata again

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -5,8 +5,8 @@ import Breadcrumb from "@/components/Common/Breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "About Page | Free Next.js Template for Startup and SaaS",
-  description: "This is About Ben Lukas and his experience in Computer Science",
+  title: "Ben's Personal Website",
+  description: "",
   // other metadata
 };
 
